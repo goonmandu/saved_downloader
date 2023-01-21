bd7be7b06474855bf9cd0b0e0aae3728575dac36
goonmandu <hwang.mingi7@gmail.com>
2023-01-21 20:01:19 +0900

    Create image similarity algorithm, yet to deploy

---
+++ image_similarity.py
@@ -0,0 +1,32 @@
+# Modified from source: https://m.blog.naver.com/codinglab9807/222711897434
+
+import cv2
+
+control = 'test_images/mahiro_wave_cropped.jpg'
+test    = 'test_images/mahiro_wave_upscaled.png'
+
+
+def determine_similarity(control, test) -> float:
+    imgs = []
+    imgs.append(cv2.imread(control))
+    imgs.append(cv2.imread(test))
+
+    hists = []
+    for img in imgs:
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        hist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+        cv2.normalize(hist, hist, 0, 1, cv2.NORM_MINMAX)
+        hists.append(hist)
+
+    query = hists[0]
+
+    methods = ['CORREL']
+
+    for index, name in enumerate(methods):
+        for i, histogram in enumerate(hists):
+            ret = cv2.compareHist(query, histogram, index)
+    return ret
+
+
+sim = determine_similarity(control, test)
+print(sim)
