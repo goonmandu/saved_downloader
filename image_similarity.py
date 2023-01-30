2722c0d653bcd52072ebf581573da76d489c036d
goonmandu <hwang.mingi7@gmail.com>
2023-01-30 14:46:35 -0500

    Make function take histograms instead of images for optimization

--- image_similarity.py
+++ image_similarity.py
@@ -2,27 +2,21 @@
 import cv2
 
 
-def determine_similarity(control: str, test: str) -> float:
-    """Determines the CORREL value of test, when compared against control."""
-    imgs = [cv2.imread(control), cv2.imread(test)]
-    hists = []
-    for img in imgs:
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        hist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
-        cv2.normalize(hist, hist, 0, 1, cv2.NORM_MINMAX)
-        hists.append(hist)
-    query = hists[0]
-    methods = ['CORREL']
-    for index, name in enumerate(methods):
-        for i, histogram in enumerate(hists):
-            ret = cv2.compareHist(query, histogram, index)
-    return ret
+def determine_similarity(control: any, test: any) -> float:
+    """Determines the CORREL value of test, when compared against control.
+
+    :param control: The histogram of the image to be compared against
+    :param test:    The histogram of the image to compare
+    :return:        The CORREL value of two histograms
+    """
+    return cv2.compareHist(control, test, 0)
 
 
 def main():
-    control = "test_images/mahiro_wave.jpg"
-    test = "test_images/najimi_stare.jpg"
-    sim = determine_similarity(control, test)
+    control = cv2.imread("test_images/mahiro_wave.jpg")
+    test = cv2.imread("test_images/najimi_stare.jpg")
+    for i in range(500):
+        sim = determine_similarity(control, test)
     print(sim)
 
 
