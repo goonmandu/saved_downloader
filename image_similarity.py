ec91aea1fc83fd336f42b3ff35767635533bccf9
goonmandu <hwang.mingi7@gmail.com>
2023-01-30 16:30:22 -0500

    Add histify function and add algorithm specifier argument to det_sim

--- image_similarity.py
+++ image_similarity.py
@@ -1,21 +1,36 @@
 # Modified from source: https://m.blog.naver.com/codinglab9807/222711897434
 import cv2
+import numpy as np
 
 
 # TODO: THIS FUNCTION DETECTS TOO MANY FALSE POSITIVES. FIND BETTER ALGORITHM.
-def determine_similarity(control: any, test: any) -> float:
-    """Determines the CORREL value of test, when compared against control.
+def determine_similarity(control: any, test: any, method: any) -> float:
+    """Determines the INTERSECT value of test, when compared against control.
 
     :param control: The histogram of the image to be compared against
     :param test:    The histogram of the image to compare
+    :param method:  The cv2 HISTCMP method identifier of algorithm
     :return:        The CORREL value of two histograms
     """
-    return cv2.compareHist(control, test, 0)
+    return cv2.compareHist(control, test, method)
+
+
+def image_histogram(path: str):
+    """Calculates the histogram from a given image path.
+
+    :param path: The path string to target image.
+    :return: The cv2 histogram object of image.
+    """
+    image = cv2.imread(path)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    cv2.normalize(hist, hist, 0, 1, cv2.NORM_MINMAX)
+    return hist
 
 
 def main():
-    control = cv2.imread("test_images/mahiro_wave.jpg")
-    test = cv2.imread("test_images/najimi_stare.jpg")
+    control = image_histogram("test_images/mahiro_wave_cropped.jpg")
+    test = image_histogram("test_images/mahiro_wave_upscaled.jpg")
     for i in range(500):
         sim = determine_similarity(control, test)
     print(sim)
