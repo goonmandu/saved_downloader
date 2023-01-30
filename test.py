095bf839088910e3b5e96f54a365f082f1cb4238
goonmandu <hwang.mingi7@gmail.com>
2023-01-30 16:31:33 -0500

    Modify similar_pictures function to take histcmp_method

--- test.py
+++ test.py
@@ -16,7 +16,7 @@ class PathAndHistogram:
 similarity_check_format_filter = ("jpg", "jpeg", "png")
 number_of_threads = os.cpu_count()
 potentially_duplicate_files = []
-THRESHOLD = 0.995
+THRESHOLD = 0.10
 
 
 # Unused, as both files will be appended to result list
@@ -29,22 +29,35 @@ def bigger_file_of_two(path1: str, path2: str) -> str:
 
 
 def part_of_list(input_list: list[any], part: int, splits: int) -> list[any]:
-    """Returns the nth 'slice' of a list from a total number of equal slices."""
+    """Returns the nth 'slice' of a list from a total number of equal slices.
+
+    :param input_list: The original list to slice.
+    :param part:       The nth slice to get.
+    :param splits:     The total number of slices to divide the list into.
+    :return: The nth slice of given list."""
     return input_list[len(input_list) * (part-1) // splits:len(input_list) * part // splits]
 
 
-def similar_pictures(existing: list[PathAndHistogram], new_downloads: list[PathAndHistogram], ret, threadno=0)\
-        -> None:
-    """Appends similar picture paths to a global variable list."""
+def similar_pictures(existing: list[PathAndHistogram], new_downloads: list[PathAndHistogram], ret: dict,
+                     histcmp_method: any, threadno=0) -> None:
+    """Appends similar picture paths to a multiprocessing manager dict argument, used to store return values.
+
+    :param existing:       List of PathAndHistogram objects of preprocessed existing images
+    :param new_downloads:  List of PathAndHistogram objects of preprocessed new images
+    :param ret:            Dict-casted multiprocessing.Manager() object for return values
+    :param histcmp_method: cv2.HISTCMP algorithm identifier to use to compare.
+    :param threadno:       The process/thread number of the function. 0 is used when not using multiprocessing.
+    :return: None (void function)
+    """
     caught_files = []
     processed = 0
     for index, new_file in enumerate(new_downloads):
         for idx_existing in range(index, len(existing)):
             if new_file.path != existing[idx_existing].path:
-                correl = determine_similarity(new_file.histogram, existing[idx_existing].histogram)
-                if correl > THRESHOLD:
+                correl = determine_similarity(new_file.histogram, existing[idx_existing].histogram, histcmp_method)
+                if correl <= THRESHOLD:
                     print(correl)
-                    caught_files.append([new_file.path, existing[idx_existing].path])
+                    caught_files.append([new_file.path, existing[idx_existing].path, correl])
         processed += 1
         print(f"Thread {threadno}: Finished comparison of file {processed} of total {len(new_downloads)}.",
               f"({round(processed / len(new_downloads), 4) * 100}%)")
@@ -80,6 +93,9 @@ if __name__ == "__main__":
     for p in processes:
         p.join()
 
-    print(return_dict)
+    for value in return_dict.values():
+        for data in value:
+            potentially_duplicate_files.append(data)
 
-    # print(potentially_duplicate_files)
+    for data in potentially_duplicate_files:
+        print(data)
